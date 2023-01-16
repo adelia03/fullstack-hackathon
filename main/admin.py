2d3709d8b05c71f0084e301b0522b2b7e7483abb
aitolkun <aitolkuns52@gmail.com>
2023-01-16 16:04:39 +0600

    models pets,child

--- main/admin.py
+++ main/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
+admin.site.register(Children)
+admin.site.register(Pets)
+admin.site.register(Narsing_House)
+admin.site.register(Children_House)
+
+
