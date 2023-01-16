b3e8f3c50badbd789bfbb25765ea931191b4ccc7
adelia <adelia.ad03@gmail.com>
2023-01-16 10:32:44 +0600

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '34.125.250.63']
 
 
 # Application definition
